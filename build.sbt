da127a83b581d004a33d72ea9dd225078fd7aec9
Philipp Ossler <philipp.ossler@gmail.com>
2017-02-07 20:57:48 +0100

    introduce scalafxml

--- build.sbt
+++ build.sbt
@@ -8,9 +8,14 @@ scalaVersion := "2.11.7"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
  
-libraryDependencies += "org.scalafx" %% "scalafx" % "8.0.102-R11"
+libraryDependencies ++= List(
+	"org.scalafx" %% "scalafx" % "8.0.102-R11",
+	"org.scalafx" %% "scalafxml-core-sfx8" % "0.3"
+)
  
 libraryDependencies ++= List(
 	"org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
 	"junit" % "junit" % "4.11" % "test"
 )
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
--- src/main/scala/de/pi/photo/frame/Main.scala
+++ src/main/scala/de/pi/photo/frame/Main.scala
@@ -7,25 +7,25 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.shape.Rectangle
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
+import scalafxml.core.DependenciesByType
+import scalafxml.core.FXMLView
 
 
 /**
  * @author Philipp
  */
 object Main extends JFXApp {
-  
-	val fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"))
-	
-	val mainView: Parent = fxmlLoader.load()
 	
 	stage = new JFXApp.PrimaryStage {
     title.value = "Pi Photo Frame"
     width = 600
     height = 450
     
-    scene = new Scene {
-    	root = mainView
-    }
+    scene = new Scene(
+        FXMLView(
+            getClass.getResource("/main.fxml"),
+            new DependenciesByType(Map()))
+    )
     
     centerOnScreen
   }
